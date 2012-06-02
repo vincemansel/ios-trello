40e6358b319c4cc6dce62a8e43ce61b08e23ed25
Vince Mansel <code@vincemansel.com>
2012-06-02 03:52:15 -0700

    Adding back the blank TrelloClientCredentials.m file.

---
+++ ios-trello/ios-trello/ios-trelloAPIClient/TrelloClientCredentials.m
@@ -0,0 +1,10 @@
+//
+//  TrelloClientCredentials.m
+//  ios-trello
+//
+
+#import "TrelloClientCredentials.h"
+
+//ios4trello
+NSString * const kTrelloConsumerKey = @"YourConsumerKey";
+NSString * const kTrelloConsumerSecretKey = @"YourConsumerSecretKey";
